37ab3f420d867ff21e2f525707a704e5570e1da4
B-Varun <bvarun662@gmail.com>
2023-08-17 21:11:33 -0700

    Adding new ToDo functionality

--- Mini Projects/todo/src/main/java/com/learn/spring/boot/todo/controller/ToDoController.java
+++ Mini Projects/todo/src/main/java/com/learn/spring/boot/todo/controller/ToDoController.java
@@ -3,11 +3,15 @@ package com.learn.spring.boot.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.learn.spring.boot.todo.task.ToDoService;
 
 @Controller
+@SessionAttributes("email")
 public class ToDoController {
 	
 	private ToDoService toDoService;
@@ -20,6 +24,17 @@ public class ToDoController {
 	public String displayToDos(ModelMap model) {
 		model.put("todos", toDoService.getToDos("Varun"));
 		return "/WEB-INF/jsp/listToDos.jsp";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String addToDo() {
+		return "/WEB-INF/jsp/createNewToDo.jsp";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String postToDo(@RequestParam String desc, ModelMap model) {
+		toDoService.addToDo(desc, model.get("email").toString());		
+		return displayToDos(model);
 	}
 	
 }
--- Mini Projects/todo/src/main/java/com/learn/spring/boot/todo/task/ToDoService.java
+++ Mini Projects/todo/src/main/java/com/learn/spring/boot/todo/task/ToDoService.java
@@ -5,22 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 public class ToDoService {
 
+	private static int count=0;
 	private static List<ToDo> todos;
 	
 	static {
 		todos = new ArrayList<>();
-		todos.add(new ToDo(1, "Learn Spring Boot", "Varun@gmail.com", LocalDate.now().plusYears(1), false));
-		todos.add(new ToDo(2, "Learn AWS", "Varun@gmail.com", LocalDate.now().plusYears(1), false));
-		todos.add(new ToDo(3, "Learn Coding", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
-		todos.add(new ToDo(4, "Algorithms", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
-		todos.add(new ToDo(5, "Data Structures", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(++count, "Learn Spring Boot", "Varun@gmail.com", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(++count, "Learn AWS", "Varun@gmail.com", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(++count, "Learn Coding", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(++count, "Algorithms", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
+		todos.add(new ToDo(++count, "Data Structures", "Bailapudi@gmail.com", LocalDate.now().plusYears(1), false));
 	}
 	
 	public List<ToDo> getToDos(String userName){
 		return todos;
+	}
+	
+	public void addToDo(String description, String email) {
+		ToDo todo = new ToDo(
+				++count,description,email,LocalDate.now().plusYears(1),false
+				);
+		todos.add(todo);
 	}
 }
